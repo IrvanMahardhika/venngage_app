ef0bbe7c936090e8044f7d514492c45a5d761842
IrvanMahardhika <irvan.mahardhika@icloud.com>
2023-09-05 05:59:15 +0700

    set initial draggable

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [squareState, setSquareState] = useState({
+		isDragging: false,
+		position: { x: 20, y: 50 },
+		startDraggingPoint: { x: 0, y: 0 },
+	});
+
+	const handleSquareDragStart = useCallback(
+		({ clientX, clientY }: { clientX: number; clientY: number }) => {
+			const startDraggingPoint = {
+				x: clientX,
+				y: clientY,
+			};
+			setSquareState((state) => ({
+				...state,
+				startDraggingPoint,
+				isDragging: true,
+			}));
+		},
+		[],
+	);
+
+	const handleSquareDrag = useCallback(
+		({ clientX, clientY }: { clientX: number; clientY: number }) => {
+			const position = {
+				x: clientX - squareState.startDraggingPoint.x + squareState.position.x,
+				y: clientY - squareState.startDraggingPoint.y + squareState.position.y,
+			};
+			setSquareState((state) => ({ ...state, position }));
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[squareState.startDraggingPoint.x, squareState.startDraggingPoint.y],
+	);
+
+	const handleSquareDragEnd = useCallback(() => {
+		setSquareState((state) => ({
+			...state,
+			isDragging: false,
+		}));
+	}, []);
+
+	return (
+		<div className='App'>
+			<div className='kanvas'>
+				<div
+					draggable={true}
+					className='square'
+					style={{
+						cursor: squareState.isDragging ? 'grabbing' : 'grab',
+						left: squareState.position.x,
+						top: squareState.position.y,
+					}}
+					onDragStart={handleSquareDragStart}
+					onDrag={handleSquareDrag}
+					onDragEnd={handleSquareDragEnd}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
